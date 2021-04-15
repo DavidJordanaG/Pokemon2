bc8fb697b3827a9875cb487154dad1a20389917c
Ernest Iglesias <ernestiglesias@gmail.com>
2021-04-15 17:01:30 +0200

    a

--- pokemonAPI/src/app/credentials.ts
+++ pokemonAPI/src/app/credentials.ts
@@ -1,8 +1,9 @@
-export const firebaseConfig = {
-    apiKey: 'Your Firebase Credentials Here',
-    authDomain: 'Your Firebase Credentials Here',
-    databaseURL: 'Your Firebase Credentials Here',
-    projectId: 'Your Firebase Credentials Here',
-    storageBucket: 'Your Firebase Credentials Here',
-    messagingSenderId: 'Your Firebase Credentials Here'
+var firebaseConfig = {
+    apiKey: "AIzaSyCjQT2GGfpw4BBSno4oE9_JjiJKfXgjit8",
+    authDomain: "pokemon-cccc2.firebaseapp.com",
+    projectId: "pokemon-cccc2",
+    storageBucket: "pokemon-cccc2.appspot.com",
+    messagingSenderId: "177851410989",
+    appId: "1:177851410989:web:3f7fd4492d6ef1149e26a4",
+    measurementId: "G-YMH7RD53W0"
   };
