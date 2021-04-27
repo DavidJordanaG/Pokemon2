068e10720a6f347165e405fc96d86c4eec7aad9b
David Jordana <58311928+DavidJordanaG@users.noreply.github.com>
2021-04-27 16:06:19 +0200

    da

--- pokemonAPI/src/app/app.component.ts
+++ pokemonAPI/src/app/app.component.ts
@@ -9,11 +9,11 @@ import { AuthenticateService } from './services/authentication.service';
 export class AppComponent {
 
   public appPages = [
-    { title: 'Log In', url: '/folder/login', icon: 'log-in' },
+  //{ title: 'Log In', url: '/folder/login', icon: 'log-in' },
     { title: 'Home', url: '/folder/home', icon: 'home' },
     { title: 'Perfil', url: '/folder/perfil', icon: 'person' },
     { title: 'Pokemons', url: '/folder/pokemon', icon: 'ribbon' },
-    { title: 'Register', url: '/folder/register', icon: 'log-in' }
+   // { title: 'Register', url: '/folder/register', icon: 'log-in' }
     
     
   ];
